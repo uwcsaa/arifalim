97101f4f13454fe1a0c51c93ec16cf7357f4b855
Maxim Zelenkin <nudepatch@gmail.com>
2020-09-21 20:06:10 +0500

    Setup Bootstrap

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,13 @@
 const { environment } = require('@rails/webpacker')
-
+const webpack = require('webpack')
 const modernizr = require('./modernizr');
+
 environment.config.merge(modernizr);
+environment.plugins.append('Provide',
+    new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery'
+    })
+)
 
 module.exports = environment
